9dc39d4f1dcedf490d21a7650931913f7e0de0f6
konghayao <3446798488@qq.com>
2022-11-07 16:33:21 +0800

    fixed: 修复 Markdown 问题

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -1,7 +1,8 @@
-import { extendsEvent, OriginComponent } from '@cn-ui/use';
+import { atomization, extendsEvent, OriginComponent } from '@cn-ui/use';
 import { createEffect, createResource, JSX } from 'solid-js';
 import { unified } from 'unified';
 export interface MarkdownProps extends JSX.HTMLAttributes<HTMLDivElement> {
+    code?: string;
     children?: string;
     remarkPlugins?: any[];
     rehypePlugins?: any[];
@@ -12,6 +13,7 @@ import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import { createIgnoreFirst } from '@cn-ui/use/src';
 export const Markdown = OriginComponent<MarkdownProps>((props) => {
+    const code = atomization(props.code??props.children)
     const [text, { refetch: rebuild }] = createResource(async () => {
         let source = unified();
         const plugins = [
@@ -25,12 +27,12 @@ export const Markdown = OriginComponent<MarkdownProps>((props) => {
         for (const i of plugins) {
             source = source.use(i);
         }
-        return source.process(props.children).then((res) => res.toString());
+        return source.process(code()).then((res) => res.toString());
     });
     createIgnoreFirst(() => {
         // 解决数据不继承问题
         rebuild();
-    }, [() => props.children]);
+    }, [code]);
     return (
         <div
             class={props.class()}
