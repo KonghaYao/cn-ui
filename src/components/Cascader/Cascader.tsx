dc97827b668339990bcec943e9d705cf43635390
江夏尧 <3446798488@qq.com>
2022-12-13 09:05:51 +0800

    refractor: 优化级联选择

--- src/components/Cascader/Cascader.tsx
+++ src/components/Cascader/Cascader.tsx
@@ -1,4 +1,4 @@
-import { createMemo, For, JSX, onMount } from 'solid-js';
+import { createMemo, createSelector, For, JSX, onMount } from 'solid-js';
 import { Atom, atomization, extendsEvent, reflect } from '@cn-ui/use';
 
 import { OriginComponent } from '@cn-ui/use';
@@ -8,6 +8,8 @@ interface CascaderProps extends JSX.HTMLAttributes<HTMLDivElement> {
     value: string[] | Atom<string[]>;
 }
 import { AtomToArray } from '@cn-ui/use';
+
+/** 级联选择器，用于选择重叠的数据 */
 export const Cascader = OriginComponent<CascaderProps, HTMLDivElement>((props) => {
     const options = AtomToArray(atomization(props.options));
 
@@ -20,23 +22,25 @@ export const Cascader = OriginComponent<CascaderProps, HTMLDivElement>((props) =
             ref={props.ref}
             {...extendsEvent(props)}
         >
+            {/* 横向的布局 */}
             <For each={options}>
                 {(subArray, index) => {
+                    const isSelected = createSelector(createMemo(() => value()[index()]));
                     return (
                         <div class="scroll-box-none flex flex-1 flex-col overflow-x-auto  ">
                             <For each={subArray()}>
                                 {(item) => {
-                                    const isSelect = createMemo(() => item === value()[index()]);
                                     return (
                                         <div
                                             class="cursor-pointer text-ellipsis whitespace-nowrap rounded-md p-2 text-center transition-colors duration-300"
                                             classList={{
-                                                'bg-slate-50': isSelect(),
-                                                'text-blue-600': isSelect(),
+                                                'bg-slate-50': isSelected(item),
+                                                'text-blue-600': isSelected(item),
                                             }}
                                             onClick={() => {
                                                 value((i) => {
                                                     const newArray = [...i];
+                                                    // 删除后面的级联选项
                                                     newArray.splice(
                                                         index(),
                                                         newArray.length - index(),
