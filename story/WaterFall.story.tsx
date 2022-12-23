5f631244cae16c3e1a23934d4bfdc08b948598ed
江夏尧 <3446798488@qq.com>
2022-12-23 15:37:34 +0800

    feat: 添加 WaterFall 响应式断点测试

--- story/WaterFall.story.tsx
+++ story/WaterFall.story.tsx
@@ -4,11 +4,24 @@ import { atom } from '@cn-ui/use';
 import { Image, LazyLoad, WaterFall } from '@cn-ui/core';
 import { photos } from './mocks/photos';
 import 'animate.css';
+import { useBreakpoints } from '@cn-ui/use';
+import { createMemo } from 'solid-js';
 
 export default (props) => {
     const items = atom(photos);
+    const { size } = useBreakpoints();
+    const column = createMemo(() => {
+        switch (size()) {
+            case 'xs':
+                return 2;
+            case 'sm':
+                return 3;
+            case 'md':
+                return 4;
+        }
+    });
     return (
-        <WaterFall items={items} column={2}>
+        <WaterFall items={items} column={column}>
             {(item) => {
                 return (
                     <LazyLoad
