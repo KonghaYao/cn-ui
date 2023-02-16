76445f50083cfecfc4f68e2715e573a67831cd26
江夏尧 <3446798488@qq.com>
2023-02-16 15:47:40 +0800

    feat: 搞定一些小问题

--- package/docs/src/components/Wrapper.tsx
+++ package/docs/src/components/Wrapper.tsx
@@ -1,6 +1,7 @@
 import { Suspense, createSignal, lazy, onMount } from 'solid-js';
 import { expose, windowEndpoint } from 'comlink';
 import '@cn-ui/core/dist/style.css';
+
 export const StroyWrapper = ({ Comp }: { Comp: string }) => {
     const Content = lazy(() => import(/* @vite-ignore */ Comp));
     const [Props, setProps] = createSignal({});
