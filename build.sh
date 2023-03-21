47689e4d9d3891e1b7db69767435234a97419ab7
江夏尧 <3446798488@qq.com>
2023-03-21 14:59:55 +0000

    fixed: 完成顺序

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # 这些按顺序即可
-# pnpm --filter "@cn-ui/reactive" build
-# pnpm --filter "@cn-ui/use" build
-# pnpm --filter "@cn-ui/transition" build
-# pnpm --filter "@cn-ui/animate" build
-# pnpm --filter "@cn-ui/headless" build
-# pnpm --filter "@cn-ui/core" build
-# pnpm --filter "@cn-ui/viewer" build
+pnpm --filter "@cn-ui/reactive" build
+pnpm --filter "@cn-ui/use" build
+pnpm --filter "@cn-ui/transition" build
+pnpm --filter "@cn-ui/animate" build
+pnpm --filter "@cn-ui/headless" build
+pnpm --filter "@cn-ui/core" build
+pnpm --filter "@cn-ui/viewer" build
 
-pnpm --filter=!@cn-ui/reactive  --filter=!@cn-ui/use --filter=!@cn-ui/transition --filter=!@cn-ui/animate --filter=!@cn-ui/headless --filter=!@cn-ui/core --filter=!@cn-ui/doc --filter=demo build
+pnpm --filter=!@cn-ui/reactive  --filter=!@cn-ui/use --filter=!@cn-ui/transition --filter=!@cn-ui/animate --filter=!@cn-ui/headless --filter=!@cn-ui/core --filter=!@cn-ui/doc --filter=!demo build
