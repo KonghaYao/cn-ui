897c0ffb47d579978ad2d11f4d5fa7a782c0bccd
江夏尧 <3446798488@qq.com>
2023-03-21 15:13:31 +0000

    fixed: 调整打包情况

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ pnpm --filter "@cn-ui/headless" build
 pnpm --filter "@cn-ui/core" build
 pnpm --filter "@cn-ui/viewer" build
 
-pnpm --filter=!@cn-ui/reactive  --filter=!@cn-ui/use --filter=!@cn-ui/transition --filter=!@cn-ui/animate --filter=!@cn-ui/headless --filter=!@cn-ui/core --filter=!@cn-ui/doc --filter=!demo build
+pnpm --filter=!@cn-ui/reactive  --filter=!@cn-ui/use --filter=!@cn-ui/transition --filter=!@cn-ui/animate --filter=!@cn-ui/headless --filter=!@cn-ui/core --filter=!docs --filter=!demo --filter=!@cn-ui/monaco build
+
+# 这个玩意的 ts 加装了 monaco 的代码，会非常大，所以设置为报错减小代码量
+pnpm --filter "@cn-ui/monaco" build
+# docs 文件夹并未打包，这个可以直接使用
