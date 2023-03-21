e249044711e7aaa19619cebf6cab401d66cdb7e1
江夏尧 <3446798488@qq.com>
2023-03-21 14:24:23 +0000

    fixed: 修复打包问题

---
+++ build.sh
@@ -0,0 +1,6 @@
+pnpm --filter "@cn-ui/reactive" build
+pnpm --filter "@cn-ui/use" build
+pnpm --filter "@cn-ui/transition" build
+pnpm --filter "@cn-ui/animate" build
+pnpm --filter "@cn-ui/headless" build
+pnpm --filter "@cn-ui/core" build
