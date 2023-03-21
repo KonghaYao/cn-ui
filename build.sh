faa2e86a7a4ebbea238a9b943adf551b30607072
江夏尧 <3446798488@qq.com>
2023-03-21 15:23:30 +0000

    fixed: 修复问题

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # 这些按顺序即可
+pnpm i
 pnpm --filter "@cn-ui/reactive" build
 pnpm --filter "@cn-ui/use" build
 pnpm --filter "@cn-ui/transition" build
@@ -12,3 +13,4 @@ pnpm --filter=!@cn-ui/reactive  --filter=!@cn-ui/use --filter=!@cn-ui/transition
 # 这个玩意的 ts 加装了 monaco 的代码，会非常大，所以设置为报错减小代码量
 pnpm --filter "@cn-ui/monaco" build
 # docs 文件夹并未打包，这个可以直接使用
+# cd package/docs && pnpm dev
