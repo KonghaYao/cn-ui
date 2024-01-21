647ebe6346f9e0e235e9cb346b768684412a20d2
江夏尧 <3446798488@qq.com>
2024-01-21 10:32:42 +0800

    refactor(Origin): 优化组件间的继承关系

--- packages/reactive/src/DOM/OriginComponent.ts
+++ packages/reactive/src/DOM/OriginComponent.ts
@@ -1,36 +1,37 @@
-import { Component, createMemo, JSX, mergeProps } from 'solid-js';
-import { classNames } from './classNames';
-import { Atom } from '../atom/atom';
+import { Component, createMemo, JSX, mergeProps } from 'solid-js'
+import { classNames } from './classNames'
+import { Atom } from '../atom/atom'
 /** OriginComponent 函数内部的输入参数修改 */
 export type OriginComponentInputType<T, RefType = HTMLElement, ModelType = string> =
     // 对内的类型注解
     Omit<T, 'style' | 'class'> & {
-        ref?: (el: RefType) => void;
-        style: JSX.CSSProperties;
-        class: typeof classNames;
-        model?: Atom<ModelType>;
+        ref?: (el: RefType) => void
+        style: JSX.CSSProperties
+        class: typeof classNames
+        model?: Atom<ModelType>
         /** 方便直接写入 v-model 的语法糖 */
         $input: () => {
-            value: ModelType;
-            'on:input': (e: { target: { value: ModelType } }) => void;
-        };
-    };
+            value: ModelType
+            'on:input': (e: { target: { value: ModelType } }) => void
+        }
+        children?: ExtractChildren<T>
+    }
 
-type ExtractChildren<T> = T extends { children: infer U } ? U : JSX.Element;
+type ExtractChildren<T> = T extends { children: infer U } ? U : JSX.Element
 /** OriginComponent 函数封装的结果函数的入参 */
 export type OriginComponentOutputType<T, RefType = HTMLElement, ModelType = string> =
     // 对外的类型注解
     Omit<T, 'style' | 'class'> & {
-        ref?: (el: RefType) => void;
-        style?: JSX.CSSProperties;
-        className?: string | string[] | typeof classNames;
-        class?: string | string[] | typeof classNames;
-        classList?: { [k: string]: boolean } | typeof classNames;
-        'v-model'?: Atom<ModelType>;
-        children?: ExtractChildren<T>;
-    };
+        ref?: (el: RefType) => void
+        style?: JSX.CSSProperties
+        className?: string | string[] | typeof classNames
+        class?: string | string[] | typeof classNames
+        classList?: { [k: string]: boolean } | typeof classNames
+        'v-model'?: Atom<ModelType>
+        children?: ExtractChildren<T>
+    }
 
-export type OriginComponentType = typeof OriginComponent;
+export type OriginComponentType = typeof OriginComponent
 
 /** 封装在组件外层，对内部提供 class style 等属性支持的装饰器 */
 export const OriginComponent = <T, RefType = HTMLElement, ModelType = string>(
@@ -41,33 +42,33 @@ export const OriginComponent = <T, RefType = HTMLElement, ModelType = string>(
         const style = createMemo<JSX.CSSProperties>(() => {
             switch (typeof props.style) {
                 case 'object':
-                    return props.style;
+                    return props.style
                 default:
-                    return {};
+                    return {}
             }
-        });
+        })
         // 类名统一转化为数组
         const classString: typeof classNames = (...args) => {
             return createMemo(() => {
-                const c = typeof props.class === 'function' ? props.class() : props.class;
-                const cn = typeof props.className === 'function' ? props.className() : props.className;
-                const cl = typeof props.classList === 'function' ? props.classList() : props.classList;
-                return classNames(c, cn, cl, ...args);
-            })();
-        };
+                const c = typeof props.class === 'function' ? props.class() : props.class
+                const cn = typeof props.className === 'function' ? props.className() : props.className
+                const cl = typeof props.classList === 'function' ? props.classList() : props.classList
+                return classNames(c, cn, cl, ...args)
+            })()
+        }
         const $input = () => ({
             value: props['v-model']?.(),
             'on:input'(e: { target: { value: ModelType } }) {
-                return props['v-model']?.(() => e.target.value);
-            },
-        });
+                return props['v-model']?.(() => e.target.value)
+            }
+        })
         let _props_ = mergeProps(props, {
             style: style(),
             class: classString,
             model: props['v-model'],
-            $input,
-        } as OriginComponentInputType<T, RefType, ModelType>);
+            $input
+        } as OriginComponentInputType<T, RefType, ModelType>)
 
-        return comp(_props_ as OriginComponentInputType<T, RefType, ModelType>);
-    };
-};
+        return comp(_props_ as OriginComponentInputType<T, RefType, ModelType>)
+    }
+}
--- packages/reactive/src/DOM/OriginDiv.tsx
+++ packages/reactive/src/DOM/OriginDiv.tsx
@@ -1,12 +1,28 @@
-import { OriginComponentInputType } from './OriginComponent';
-import { extendsEvent } from './extendsEvent';
-import { JSXElement } from 'solid-js';
-
+import { OriginComponent, OriginComponentInputType } from './OriginComponent'
+import { extendsEvent } from './extendsEvent'
 /** 自动继承组件属性  */
-export const OriginDiv = <T, RefType = HTMLElement, ModelType = string>(props: { prop: OriginComponentInputType<T, RefType, ModelType>; children?: JSXElement }) => {
-    return (
-        <div ref={(el) => props.prop.ref && props.prop.ref(el as RefType)} class={props.prop.class('cn-sortable')} style={props.prop.style} {...extendsEvent(props.prop)}>
-            {props.children}
-        </div>
-    );
-};
+export const OriginDiv = OriginComponent(
+    <T, _RefType, ModelType>(
+        props: OriginComponentInputType<
+            {
+                prop: OriginComponentInputType<T, HTMLDivElement, ModelType>
+            },
+            HTMLDivElement
+        >
+    ) => {
+        return (
+            <div
+                ref={(el) => {
+                    props.prop.ref && props.prop.ref(el)
+                    props.ref && props.ref(el)
+                }}
+                class={props.prop.class('cn-origin-div', props.class())}
+                style={{ ...props.style, ...props.prop.style }}
+                {...extendsEvent(props)}
+                {...extendsEvent(props.prop)}
+            >
+                {props.children}
+            </div>
+        )
+    }
+)
