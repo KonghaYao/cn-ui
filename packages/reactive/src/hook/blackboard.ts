983843673e741dfc5b142ba4bcf48dc9e7b8b553
江夏尧 <3446798488@qq.com>
2024-01-21 17:47:55 +0800

    feat(useSelect): 添加 disabled 状态管理

--- packages/reactive/src/hook/blackboard.ts
+++ packages/reactive/src/hook/blackboard.ts
@@ -1,37 +1,37 @@
-import { createContext, useContext } from 'solid-js';
+import { createContext, useContext } from 'solid-js'
 
 /**
  * @zh Blackboard 是用于大型软件中的 api 互通系统，用于解决 Context 不能兄弟互通数据的问题。可以全局注册，也可局部注册
  *
  */
 export const createBlackBoard = <T extends Record<string, any>>() => {
-    const store = new Map();
+    const store = new Map()
     return {
         /** 应该在组件声明时进行注册 App，保证在 onMount 时能够获取到数据 */
         register<D extends keyof T>(name: D, api: T[D]) {
             if (store.has(name)) {
-                throw new Error('Blackboard has a same app named ' + name.toString());
+                throw new Error('Blackboard has a same app named ' + name.toString())
             }
-            return store.set(name, api);
+            return store.set(name, api)
         },
         /** 在 onMount 阶段可以获取到所有声明的 App */
         getApp<D extends keyof T>(name: D): T[D] {
-            if (!store.has(name)) throw new Error(`Blackboard app ${name.toString()} isn't init yet`);
-            return store.get(name);
+            if (!store.has(name)) throw new Error(`Blackboard app ${name.toString()} isn't init yet`)
+            return store.get(name)
         },
         delete(name: keyof T) {
-            if (!store.has(name)) throw new Error(`Blackboard app ${name.toString()} isn't init yet`);
-            store.delete(name);
+            if (!store.has(name)) throw new Error(`Blackboard app ${name.toString()} isn't init yet`)
+            store.delete(name)
         },
         /** 检查APP是否注册*/
         check(name: keyof T) {
-            return store.has(name);
+            return store.has(name)
         },
         destroy() {
-            store.clear();
-        },
-    };
-};
+            store.clear()
+        }
+    }
+}
 
 /**
  * @zh Context 和 useContext 一体化
@@ -49,11 +49,15 @@ export const createBlackBoard = <T extends Record<string, any>>() => {
  *
  *  */
 export const createCtx = <T>(data?: T) => {
-    const ctx = createContext(data);
+    const ctx = createContext(data)
     return {
         ...ctx,
         use() {
-            return useContext(ctx);
-        },
-    };
-};
+            const context = useContext(ctx)
+            if (context === undefined) {
+                throw new Error('context is empty: place check parent jsx element!')
+            }
+            return context as T
+        }
+    }
+}
--- packages/reactive/src/hook/useSelect.ts
+++ packages/reactive/src/hook/useSelect.ts
@@ -1,62 +1,66 @@
-import { atomization } from '../utils';
-import { ArrayAtom, atom, Atom } from '../atom/index';
-import { createEffect } from 'solid-js';
+import { atomization } from '../utils'
+import { ArrayAtom, atom, Atom, SetAtom } from '../atom/index'
+import { createEffect } from 'solid-js'
 
 /**
- * @zh 多选状态管理 */
+ * @zh 多选状态管理
+ * */
 export const useSelect = function (
     props: {
-        activeIds?: Atom<string[]>;
+        activeIds?: Atom<string[]>
         /** 默认多选，但是可以取消 */
-        multi?: Atom<boolean> | boolean;
+        multi?: Atom<boolean> | boolean
     } = {}
 ) {
-    const activeIdsArray = ArrayAtom(atomization(props.activeIds ?? []));
+    const activeIdsArray = ArrayAtom(atomization(props.activeIds ?? []))
 
-    const multi = atomization(props.multi ?? true);
-    const activeIdsSet = atom(new Set(activeIdsArray()));
+    const multi = atomization(props.multi ?? true)
+    const activeIdsSet = atom(new Set(activeIdsArray()))
+
+    const disabledSet = SetAtom<string>([])
 
     // 自动强制单选，防止上流 activeIds 强制多选的行为
     createEffect(() => {
-        !multi() && activeIdsArray((i) => i.slice(0, 1));
-        activeIdsSet(() => new Set(activeIdsArray()));
-    });
+        !multi() && activeIdsArray((i) => i.slice(0, 1))
+        activeIdsSet(() => new Set(activeIdsArray()))
+    })
 
     /** 曾注册过的状态 */
-    const allRegistered = atom(new Set<string>(), { equals: false });
+    const allRegistered = atom(new Set<string>(), { equals: false })
 
     /** 更改相应 id 的状态 */
     const changeSelected = (id: string, state?: boolean) => {
+        if (disabledSet().has(id)) return
         // 默认自动置反
-        if (state === undefined) state = !activeIdsSet().has(id);
+        if (state === undefined) state = !activeIdsSet().has(id)
 
         if (state === true && !activeIdsSet().has(id)) {
             activeIdsArray((i) => {
                 if (multi()) {
-                    return [...i, id];
+                    return [...i, id]
                 } else {
-                    return [id];
+                    return [id]
                 }
-            });
+            })
         } else if (state === false) {
-            activeIdsArray.removeAll(id);
+            activeIdsArray.removeAll(id)
         }
-    };
+    }
     return {
         /** 更改状态 */
         changeSelected,
         /** 注册键，可以在任何时候进行注册 */
         register(id: string, state = false) {
             allRegistered((i) => {
-                i.add(id);
-                return i;
-            });
-            changeSelected(id, state);
+                i.add(id)
+                return i
+            })
+            changeSelected(id, state)
         },
         /** 取消整个键的采用 */
         deregister(id: string) {
-            activeIdsArray.removeAll(id);
-            allRegistered((i) => (i.delete(id), i));
+            activeIdsArray.removeAll(id)
+            allRegistered((i) => (i.delete(id), i))
         },
         /** 所有注册过的键 */
         allRegistered,
@@ -64,7 +68,9 @@ export const useSelect = function (
         activeIds: activeIdsSet,
         /** 检查一个键是否被选中 */
         isSelected: (id: string) => {
-            return activeIdsSet().has(id);
+            return activeIdsSet().has(id)
         },
-    };
-};
+        /** 禁用 */
+        disabledSet
+    }
+}
