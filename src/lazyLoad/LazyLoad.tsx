d53300e95232170a9a753393fd350637289a5f6a
江夏尧 <3446798488@qq.com>
2024-01-21 10:44:13 +0800

    feat(lazyLoad): 添加 lazyLoad 的 debounce 处理

--- src/lazyLoad/LazyLoad.tsx
+++ src/lazyLoad/LazyLoad.tsx
@@ -1,61 +1,62 @@
-import { Atom, atom, DefaultAC, extendsEvent, OriginComponent, OriginComponentInputType } from '@cn-ui/reactive';
-import { debounce } from 'lodash-es';
-import { children as getChildren, Component, createEffect, JSXElement, lazy, onMount, Suspense, Show, mergeProps } from 'solid-js';
+import { Atom, atom, DefaultAC, extendsEvent, OriginComponent, OriginComponentInputType } from '@cn-ui/reactive'
+import { debounce } from 'lodash-es'
+import { children as getChildren, Component, createEffect, JSXElement, lazy, onMount, Suspense, Show, mergeProps } from 'solid-js'
 
 interface LazyLoadProps<T extends Record<string, Component | any>> extends IntersectionObserverInit {
     /** 未进入 loading 态时的操作 */
-    children?: JSXElement;
-    load: () => Promise<T>;
-    loadKey?: keyof T;
+    children?: JSXElement
+    load: () => Promise<T>
+    loadKey?: keyof T
     /** 加载态 */
-    loading?: Component;
-    error?: Component;
+    loading?: Component
+    error?: Component
     /** 是否只在第一次进行加载 */
-    once?: boolean;
+    once?: boolean
+    debounceTime?: number
 }
 
 export const LazyLoad = OriginComponent(function <T extends Record<string, Component | any>>(props: OriginComponentInputType<LazyLoadProps<T>>) {
     props = mergeProps(
         {
             loading: DefaultAC.loading,
-            error: DefaultAC.error,
+            error: DefaultAC.error
         } as OriginComponentInputType<LazyLoadProps<T>>,
         props
-    );
-    const fallback = getChildren(() => props.children);
-    const visible = atom(false);
-    let observer: IntersectionObserver;
-    const ref = atom<HTMLDivElement | null>(null);
+    )
+    const fallback = getChildren(() => props.children)
+    const visible = atom(false)
+    let observer: IntersectionObserver
+    const ref = atom<HTMLDivElement | null>(null)
     onMount(() => {
-        if (observer) observer.disconnect();
+        if (observer) observer.disconnect()
         // 处理用户手速太快划过去的问题
-        const visibleDebounce = debounce(visible, 150) as any as Atom<boolean>;
+        const visibleDebounce = debounce(visible, props.debounceTime ?? 150, { leading: false }) as any as Atom<boolean>
         observer = new IntersectionObserver(([entries]) => {
-            visibleDebounce(entries.isIntersecting);
-        }, props);
-        observer.observe(ref()!);
-    });
+            visibleDebounce(entries.isIntersecting)
+        }, props)
+        observer.observe(ref()!)
+    })
     props.once &&
         createEffect(() => {
-            visible() && observer && observer.disconnect();
-        });
+            visible() && observer && observer.disconnect()
+        })
     const item = (
         <Suspense fallback={props.loading && props.loading({})}>
             {lazy(async () => {
                 try {
-                    const modules = await props.load();
-                    return { default: modules[props.loadKey ?? 'default'] as Component };
+                    const modules = await props.load()
+                    return { default: modules[props.loadKey ?? 'default'] as Component }
                 } catch (e) {
-                    return { default: props.error! };
+                    return { default: props.error! }
                 }
             })({})}
         </Suspense>
-    );
+    )
     return (
         <div class={props.class()} style={props.style} {...extendsEvent(props)} ref={ref}>
             <Show when={visible()} fallback={fallback as any}>
                 {item}
             </Show>
         </div>
-    );
-});
+    )
+})
--- src/lazyLoad/index.stories.tsx
+++ src/lazyLoad/index.stories.tsx
@@ -1,38 +1,38 @@
-import type { Meta, StoryObj } from 'storybook-solidjs';
+import type { Meta, StoryObj } from 'storybook-solidjs'
 
-import { LazyLoad } from './index';
-import { DefineAC, sleep } from '@cn-ui/reactive';
+import { LazyLoad } from './index'
+import { DefineAC, sleep } from '@cn-ui/reactive'
 
 const meta = {
     title: 'Utils/LazyLoad',
     component: LazyLoad,
     tags: ['autodocs'],
-    argTypes: {},
-} satisfies Meta<typeof LazyLoad>;
+    argTypes: {}
+} satisfies Meta<typeof LazyLoad>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 DefineAC({
     loading: () => <div class="h-32 w-32 flex justify-center items-center bg-yellow-600">加载中</div>,
-    error: () => <div class="h-32 w-32 flex justify-center items-center bg-rose-600">加载失败</div>,
-});
+    error: () => <div class="h-32 w-32 flex justify-center items-center bg-rose-600">加载失败</div>
+})
 
 /**  */
 export const Primary: Story = {
     render() {
-        sleep;
+        sleep
         return (
             <LazyLoad
                 load={async () => {
-                    await sleep(1000);
-                    return import('./example/sample');
+                    await sleep(1000)
+                    return import('./example/sample')
                 }}
                 loadKey="Sample"
             >
                 <div class="h-32 w-32 flex justify-center items-center bg-purple-600">未加载</div>
             </LazyLoad>
-        );
+        )
     },
-    args: {},
-};
+    args: {}
+}
