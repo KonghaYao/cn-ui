21cb5f32c8ce7332ec4d883652b234517a8811e2
江夏尧 <3446798488@qq.com>
2024-02-08 08:21:40 +0800

    feat(icon): add spin

--- src/icon/Icon.tsx
+++ src/icon/Icon.tsx
@@ -1,11 +1,14 @@
 import { OriginComponent, extendsEvent } from '@cn-ui/reactive'
 import { JSXElement } from 'solid-js'
 
-export const Icon = OriginComponent<{ children: JSXElement }>((props) => {
+export const Icon = OriginComponent<{
+    spin?: boolean
+    children: JSXElement
+}>((props) => {
     return (
         <span
             role="img"
-            class={props.class('inline-flex align-center text-center leading-[0]')}
+            class={props.class(props.spin && 'animate-spin', 'inline-flex align-center text-center leading-[0]')}
             style={{
                 ...props.style,
                 'vertical-align': '-0.125em'
