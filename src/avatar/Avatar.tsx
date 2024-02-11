08decac15a2f3e5a6d06c5669e80946ad9fb34bb
江夏尧 <3446798488@qq.com>
2024-02-11 20:52:24 +0800

    feat(chatbox): 更新组件

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -1,7 +1,7 @@
 import { Avatar } from '@ark-ui/solid'
 import { DefaultAC, JSXSlot, ensureFunctionResult } from '@cn-ui/reactive'
 
-const Basic = (props: { fallback?: JSXSlot; src: string }) => (
+const Basic = (props: { fallback?: JSXSlot; src?: string }) => (
     <Avatar.Root>
         <Avatar.Fallback>{ensureFunctionResult(props.fallback ?? DefaultAC.fallback)}</Avatar.Fallback>
         <Avatar.Image src={props.src} alt="avatar" class="rounded-full" />
--- src/chatBox/ChatBox.tsx
+++ src/chatBox/ChatBox.tsx
@@ -1,9 +1,12 @@
-import { JSXSlot, OriginComponent, OriginComponentInputType, OriginDiv, ensureFunctionResult } from '@cn-ui/reactive'
+import { JSXSlot, OriginComponent, OriginComponentInputType, OriginDiv, atom, ensureFunctionResult } from '@cn-ui/reactive'
 import { VirtualList, VirtualListProps } from '../virtualList'
 import { Col, Row } from '../RowAndCol'
+import { BaseInput } from '../control/input'
+import { Avatar } from '../avatar'
 
 export interface ChatBoxMessage {
     id: string
+    avatar?: string
     message: JSXSlot
     position?: 'right' | 'left'
     create_at?: string
@@ -11,26 +14,34 @@ export interface ChatBoxMessage {
 export interface ChatBoxProps<T extends ChatBoxMessage> extends Pick<VirtualListProps<T>, 'each' | 'estimateSize'> {}
 
 export const ChatBox = OriginComponent(function <T extends ChatBoxMessage>(props: OriginComponentInputType<ChatBoxProps<T>, HTMLDivElement, T[]>) {
+    const userMessage = atom('')
     return (
         <OriginDiv prop={props} class="flex flex-col h-full ">
+            <header class="h-16 bg-gray-100"></header>
             <VirtualList reverse each={props.each} getItemKey={(i) => props.each[i].id}>
                 {(item) => {
                     return (
                         <Row bottomSpace={10}>
                             <Col span={4}>
-                                <div class="aspect-square bg-gray-100 h-8"></div>
+                                <Avatar src={item.avatar} fallback={<div>A</div>}></Avatar>
                             </Col>
                             <Col span={16}>
                                 <div class="bg-gray-100 p-2 rounded-md">{ensureFunctionResult(item.message)}</div>
                             </Col>
                             <Col span={4}>
-                                <div class="aspect-square bg-gray-100 h-8"></div>
+                                <Avatar src={item.avatar} fallback={<div>A</div>}></Avatar>
                             </Col>
                         </Row>
                     )
                 }}
             </VirtualList>
-            <div class="h-32 bg-gray-100"></div>
+            <Row class="h-24 bg-gray-100" gutter={[4, 12]}>
+                <Col span={2}></Col>
+                <Col span={20}>
+                    <BaseInput resize={false} class="bg-white h-full w-full" type="textarea" v-model={userMessage}></BaseInput>
+                </Col>
+                <Col span={2}></Col>
+            </Row>
         </OriginDiv>
     )
 })
