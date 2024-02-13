b4a77bf6210c663f04d33b2b8efca95b88adc906
江夏尧 <3446798488@qq.com>
2024-02-13 08:51:48 +0800

    feat(reactive): mapper 作用域函数支持

--- packages/reactive/src/transformer/useMapper.ts
+++ packages/reactive/src/transformer/useMapper.ts
@@ -1,13 +1,14 @@
 import { computed } from '../atom/reflect'
+import { ensureFunctionResult } from '../utils'
 
 /**
  * switch 逻辑的简化设计模式
  * @example
  * const direction = useMapper(props.direction ?? 'col', {
  *     row: 'flex-row',
- *     col: 'flex-col'
+ *     col: ()=> 'flex-col'
  * })
  */
-export function useMapper<T extends keyof D, D>(type: T | (() => T), Mapper: D) {
-    return computed(() => (typeof type === 'function' ? Mapper[type()] : Mapper[type]))
+export function useMapper<T extends keyof D, D extends Record<string, unknown | ((this: D) => unknown)>>(type: T | (() => T), Mapper: D) {
+    return computed(() => ensureFunctionResult(typeof type === 'function' ? Mapper[type()] : Mapper[type], undefined, Mapper))
 }
--- packages/reactive/src/utils/ensureFunctionResult.ts
+++ packages/reactive/src/utils/ensureFunctionResult.ts
@@ -2,10 +2,10 @@ type ensureParameters<T> = T extends (...args: any) => any ? Parameters<T> : nev
 type ensureReturnType<T> = T extends (...args: any) => any ? ReturnType<T> : never
 
 /** 从构造函数和原始数据中统一为原始数据 */
-export const ensureFunctionResult = <T>(result: T, props?: ensureParameters<T>): ensureReturnType<T> => {
+export const ensureFunctionResult = <T>(result: T, props?: ensureParameters<T>, bindThis?: unknown): ensureReturnType<T> => {
     if (typeof result === 'function') {
         // @ts-ignore
-        return result(...(props ?? []))
+        return result.apply(bindThis, props ?? [])
     }
     /** @ts-ignore */
     return result
