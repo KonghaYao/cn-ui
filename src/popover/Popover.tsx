d02f41a31aa657fb7bf9cd398ef8efce24b29d93
江夏尧 <3446798488@qq.com>
2024-02-22 21:45:19 +0800

    feat(popover): popover 支持多个子孙

--- src/popover/Popover.tsx
+++ src/popover/Popover.tsx
@@ -36,38 +36,41 @@ const _Popover = OriginComponent<PopoverProps, HTMLDivElement, boolean>((props) 
     )
     const c = children(() => props.children)
     const child = createMemo(() => {
-        const doms = c.toArray()
-        if (doms.length !== 1) throw new Error('Popover need only 1 child!')
-        return doms[0] as HTMLElement
+        return c.toArray()[0] as HTMLElement
     })
     return (
-        <Popover.Root
-            initialFocusEl={props.initialFocusEl}
-            autoFocus={false}
-            closeOnInteractOutside={true}
-            portalled
-            lazyMount={true}
-            open={props.model()}
-            positioning={positioning()}
-            onOpenChange={(value) => props.model(value.open)}
-        >
-            <PopoverTrigger as={child}></PopoverTrigger>
-            <Portal>
-                <Popover.Positioner>
-                    <Popover.Content class="popover__content outline-none bg-design-card rounded-md flex flex-col z-50 p-2">
-                        <Popover.Arrow class="popover__arrow">
-                            <Popover.ArrowTip class="popover__arrowTip" />
-                        </Popover.Arrow>
-                        <div>{ensureFunctionResult(props.content)}</div>
-                    </Popover.Content>
-                </Popover.Positioner>
-            </Portal>
-        </Popover.Root>
+        <>
+            <Popover.Root
+                initialFocusEl={props.initialFocusEl}
+                autoFocus={false}
+                closeOnInteractOutside={true}
+                portalled
+                lazyMount={true}
+                open={props.model()}
+                positioning={positioning()}
+                onOpenChange={(value) => props.model(value.open)}
+            >
+                <PopoverTrigger as={child}></PopoverTrigger>
+                <Portal>
+                    <Popover.Positioner>
+                        <Popover.Content class="popover__content outline-none bg-design-card rounded-md flex flex-col z-50 p-2">
+                            <Popover.Arrow class="popover__arrow">
+                                <Popover.ArrowTip class="popover__arrowTip" />
+                            </Popover.Arrow>
+                            <div>{ensureFunctionResult(props.content)}</div>
+                        </Popover.Content>
+                    </Popover.Positioner>
+                </Portal>
+            </Popover.Root>
+            {/* 只渲染第一个子元素为 触发器，其余渲染为额外元素 */}
+            {c.toArray().slice(1)}
+        </>
     )
 })
 export { _Popover as Popover }
 export const PopoverTrigger = (props: { as: () => HTMLElement }) => {
     const api = usePopoverContext()
+    if (!props.as()) return null
     spread(props.as(), api().triggerProps)
     return props.as()
 }
