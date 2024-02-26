a0d7c556c221300badfa0d0648dde938672bb763
江夏尧 <3446798488@qq.com>
2024-02-26 21:27:14 +0800

    feat(date-picker): 完成时间面板

--- src/datePicker/index.stories.tsx
+++ src/datePicker/index.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from 'storybook-solidjs'
 
 import { DatePanel } from './index'
+import { atom } from '@cn-ui/reactive'
 
 const meta = {
     title: 'Controls/DatePanel 日期选择',
@@ -13,9 +14,11 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
     render() {
+        const date = atom([])
         return (
             <>
-                <DatePanel></DatePanel>
+                {date().map((i) => i)}
+                <DatePanel v-model={date}></DatePanel>
             </>
         )
     },
