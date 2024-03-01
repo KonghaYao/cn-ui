fa29c01f53ed1a34d9db8a9b0ca4bd5015d7cf96
3446798488 <3446798488@qq.com>
2024-03-01 05:44:42 +0000

    feat(data-viewer): 添加 json 展示器

---
+++ src/dataViewer/index.tsx
@@ -0,0 +1,12 @@
+import '@alenaksu/json-viewer'
+import { Flex } from '../container'
+import './json-viewer.css'
+export const JSONViewer = (props: { data: any }) => {
+    return (
+        <Flex vertical align="start" class="border relative p-2 rounded-lg border-gray-400">
+            <div class="absolute -top-3 right-4"> JSON Viewer</div>
+            {/* @ts-ignore */}
+            <json-viewer data={{ ...props.data }}></json-viewer>
+        </Flex>
+    )
+}
