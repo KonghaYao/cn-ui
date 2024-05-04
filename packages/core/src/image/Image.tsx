41700dd51e7931d1386a0408aa853504c42c215c
江夏尧 <3446798488@qq.com>
2024-05-04 14:57:24 +0800

    feat(image): 添加错误处理功能

--- packages/core/src/image/Image.tsx
+++ packages/core/src/image/Image.tsx
@@ -1,20 +1,45 @@
-import { type JSX, mergeProps } from "solid-js";
+import { type JSX, mergeProps, Show } from "solid-js";
 
-import { OriginComponent, extendsEvent } from "@cn-ui/reactive";
+import {
+    type JSXSlot,
+    OriginComponent,
+    computed,
+    extendsEvent,
+    atom,
+    ensureFunctionResult,
+} from "@cn-ui/reactive";
 export type ImageFit = "contain" | "cover" | "fill" | "none" | "scale-down";
 export type ImagePosition = "center" | "top" | "right" | "bottom" | "left" | string;
 export interface ImageProps extends JSX.HTMLAttributes<HTMLImageElement> {
     src: string;
+    alt: string;
     fit?: ImageFit;
     position?: ImagePosition;
     round?: boolean;
     block?: boolean;
     width?: number;
     height?: number;
-    alt: string;
+
+    /**
+     * @example [
+     *    [
+     *        "https://plus.unsplash.com/premium_photo-1674939148088-d71acc1541ff?w=100&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
+     *        100,
+     *    ],
+     *    [
+     *        "https://plus.unsplash.com/premium_photo-1674939148088-d71acc1541ff?w=200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
+     *        200,
+     *    ],
+     * ]
+     */
+    srcSets?: [string, number][];
     radius?: number;
+    fallback?: string | JSXSlot;
 }
 
+/**
+ * 图片预设组件
+ */
 export const Image = OriginComponent<ImageProps, HTMLImageElement>((props) => {
     props = mergeProps(
         {
@@ -23,20 +48,56 @@ export const Image = OriginComponent<ImageProps, HTMLImageElement>((props) => {
         },
         props,
     );
+    const srcSets = computed(
+        () =>
+            props.srcSets
+                ?.map(([src, size], index) => {
+                    if (index === props.srcSets!.length - 1) return `${src} ${size}w`;
+                    return `${src} ${size - 1}w`;
+                })
+                .join(","),
+    );
+    const sizes = computed(
+        () =>
+            props.srcSets
+                ?.map(([_, size], index) => {
+                    if (index === props.srcSets!.length - 1) {
+                        return `${size}px`;
+                    }
+                    return `(max-width: ${size}px) ${size - 1}px`;
+                })
+                .join(","),
+    );
+    const isError = atom(false);
+    const isStringFallback = computed(() => typeof props.fallback === "string");
     return (
-        // biome-ignore lint/a11y/useAltText: <explanation>
-        <img
-            height={props.height}
-            width={props.width}
-            class={props.class("cn-image m-auto h-full")}
-            style={{
-                ...props.style,
-                "object-fit": props.fit,
-                "object-position": props.position,
-            }}
-            src={props.src}
-            alt={props.alt}
-            {...extendsEvent(props)}
-        />
+        <>
+            {/* biome-ignore lint/a11y/useAltText: <explanation> */}
+            <img
+                height={props.height}
+                width={props.width}
+                class={props.class(
+                    "cn-image m-auto h-full",
+                    isError() && !isStringFallback() && "hidden",
+                )}
+                style={{
+                    ...props.style,
+                    "object-fit": props.fit,
+                    "object-position": props.position,
+                }}
+                src={isError() && isStringFallback() ? (props.fallback as string) : props.src}
+                sizes={sizes()}
+                srcset={srcSets()}
+                alt={props.alt}
+                {...extendsEvent(props)}
+                /** @ts-ignore */
+                on:error={() => {
+                    isError(true);
+                }}
+            />
+            <Show when={isError() && !isStringFallback()}>
+                {ensureFunctionResult(props.fallback)}
+            </Show>
+        </>
     );
 });
--- packages/core/src/image/index.stories.tsx
+++ packages/core/src/image/index.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "storybook-solidjs";
 
 import { photos } from "../waterFall/example/photos";
 import { Image } from "./index";
+import { Alert } from "../Message";
 
 const meta = {
     title: "Basic 基础组件/Image",
@@ -15,7 +16,41 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
     render() {
-        return <Image src={photos[0].src} />;
+        return <Image src={photos[0].src} alt="any" />;
+    },
+    args: {},
+};
+
+/** 暂时未进行测试 */
+export const Responsive: Story = {
+    name: "Responsive 响应式图片",
+    render() {
+        const images: [string, number][] = [
+            [photos[0].src, 400],
+            [photos[1].src, 500],
+            [photos[2].src, 600],
+        ];
+        return (
+            <>
+                <Image src={photos[0].src} srcSets={images} alt="bea" />
+            </>
+        );
+    },
+    args: {},
+};
+export const Fallback: Story = {
+    name: "Fallback 错误处理",
+    render() {
+        return (
+            <>
+                <Image src="3483483478" fallback={photos[0].src} alt="any" />
+                <Image
+                    src="3483483478"
+                    fallback={<Alert message="image-error-fallback" />}
+                    alt="any"
+                />
+            </>
+        );
     },
     args: {},
 };
